d84e27508215ae960e3a88559b370633be229232
defevan <evanjones2024@icloud.com>
2024-12-12 00:47:50 -0800

    fmt

--- day12.rkt
+++ day12.rkt
@@ -3,11 +3,11 @@
 (define (fmt fn)
   (define ht (make-hash))
   (define lines (file->lines fn))
-  (for/list ((l lines)
-             (linen (length lines)))
+  (for/list ([l lines]
+             [linen (length lines)])
     (define chars (filter non-empty-string? (string-split l "")))
-    (for/list ((c chars)
-               (charn (length chars)))
+    (for/list ([c chars]
+               [charn (length chars)])
       (hash-set! ht (list linen charn) c)))
   ht)
 
@@ -21,31 +21,32 @@
 
 (define (region input r key value)
   (hash-set! r key value)
-  (for/list ((t (list up right down left))
+  (for/list ([t (list up right down left)]
              #:do ((define next-key (go t key)))
              #:when (hash-has-key? input next-key))
     (define next-value (hash-ref input next-key))
-    (cond ((hash-has-key? r next-key) empty)
-          ((equal? value next-value) (region input r next-key next-value))))
+    (cond
+      [(hash-has-key? r next-key) empty]
+      [(equal? value next-value) (region input r next-key next-value)]))
   r)
 
 (define (regions input)
-  (for/fold ((lst '()))
-            ((key (hash-keys input))
+  (for/fold ([lst '()])
+            ([key (hash-keys input)]
              #:when (not (ormap (curryr hash-has-key? key) lst)))
     (cons (region input (make-hash) key (hash-ref input key)) lst)))
 
 (define (hash-ref-safe ht key)
-  (if (hash-has-key? ht key) (hash-ref ht key) empty))
+  (if (hash-has-key? ht key)
+      (hash-ref ht key)
+      empty))
 
 (define (perimeter input r)
-  (for*/sum ((key (hash-keys r))
-             (t (list up right down left)))
-    (if (equal? (hash-ref r key) (hash-ref-safe input (go t key))) 0 1)))
-    
+  (for*/sum ((key (hash-keys r)) (t (list up right down left)))
+            (if (equal? (hash-ref r key) (hash-ref-safe input (go t key))) 0 1)))
+
 (define (part1 input)
-  (for/sum ((r (regions input)))
-    (* (hash-count r) (perimeter input r))))
+  (for/sum ((r (regions input))) (* (hash-count r) (perimeter input r))))
 
 (define (corner? input h v key value)
   (and (equal? (hash-ref-safe input (go h key)) value)
@@ -58,13 +59,14 @@
 
 (define (turns input region value)
   (for/sum ((key (hash-keys region)))
-    (count identity
-           (for*/list ((h (list left right)) (v (list up down)) (f (list corner? side?)))
-             (f input h v key value)))))
+           (count identity
+                  (for*/list ([h (list left right)]
+                              [v (list up down)]
+                              [f (list corner? side?)])
+                    (f input h v key value)))))
 
 (define (part2 input)
-  (for/sum ((r (regions input)))
-    (* (hash-count r) (turns input r (first (hash-values r))))))
+  (for/sum ((r (regions input))) (* (hash-count r) (turns input r (first (hash-values r))))))
 
 (module+ test
   (require rackunit)
@@ -72,3 +74,7 @@
   (check-equal? (part1 (fmt "static/day12input.txt")) 1456082)
   (check-equal? (part2 (fmt "static/day12example.txt")) 80)
   (check-equal? (part2 (fmt "static/day12input.txt")) 872382))
+
+(module+ main
+  (define input (fmt "static/day12input.txt"))
+  (printf "day12\n\tpart1: ~a\n\tpart2: ~a\n" (part1 input) (part2 input)))
