15d0c032bec9707e35bb3f5a43dd68812579a463
defevan <evanjones2024@icloud.com>
2024-12-14 00:54:43 -0800

    Smaller 14p2.

--- day14.rkt
+++ day14.rkt
@@ -6,65 +6,37 @@
       (map string->number (cdr (regexp-match #px"p=(\\d+),(\\d+)+ v=(-?\\d+),(-?\\d+)" l))))
     (list (list px py) (list vx vy))))
 
-(define (quad? min-w max-w min-h max-h p)
-  (and (>= max-w (first p) min-w) (>= max-h (last p) min-h)))
+(define (in-bounds? min-w max-w min-h max-h r)
+  (match-define (list (list px py) (list vx vy)) r)
+  (and (>= max-w px min-w) (>= max-h py min-h)))
+
+(define (blink w h r)
+  (match-define (list (list px py) (list vx vy)) r)
+  (list (list (modulo (+ px vx) w) (modulo (+ py vy) h)) (list vx vy)))
 
 (define (part1 input w h c)
-  (define result
-    (map car
-         (for*/fold ([robots input]) ([i c])
-           (for/list ([r robots])
-             (match-define (list p v) r)
-             (list (list (modulo (+ (first p) (first v)) w) (modulo (+ (last p) (last v)) h)) v)))))
+  (define robots
+    (for*/fold ([robots input]) ([i c])
+      (for/list ([r robots])
+        (blink w h r))))
   (define quads
-    (list (curry quad? 0 (sub1 (floor (/ w 2))) 0 (sub1 (floor (/ h 2))))
-          (curry quad? (add1 (floor (/ w 2))) w 0 (sub1 (floor (/ h 2))))
-          (curry quad? 0 (sub1 (floor (/ w 2))) (add1 (floor (/ h 2))) h)
-          (curry quad? (add1 (floor (/ w 2))) w (add1 (floor (/ h 2))) h)))
-  (for/product ((f quads)) (for/sum ((r result) #:when (f r)) 1)))
+    (list (curry in-bounds? 0 (sub1 (floor (/ w 2))) 0 (sub1 (floor (/ h 2))))
+          (curry in-bounds? (add1 (floor (/ w 2))) w 0 (sub1 (floor (/ h 2))))
+          (curry in-bounds? 0 (sub1 (floor (/ w 2))) (add1 (floor (/ h 2))) h)
+          (curry in-bounds? (add1 (floor (/ w 2))) w (add1 (floor (/ h 2))) h)))
+  (for/product ((f quads)) (for/sum ((r robots) #:when (f r)) 1)))
 
-(define transforms
-  (list (list add1 identity)
-        (list sub1 identity)
-        (list identity add1)
-        (list identity sub1)
-        (list add1 add1)
-        (list add1 sub1)
-        (list sub1 add1)
-        (list sub1 sub1)))
-
-(define (move dir pos)
-  (list ((first dir) (first pos)) ((last dir) (last pos))))
-
-(define (touching-count ht seen key)
-  (hash-set! seen key #t)
-  (for/sum ((t transforms) #:do ((define next (move t key)))
-                           #:when (and (hash-has-key? ht next) (not (hash-has-key? seen next))))
-           (add1 (touching-count ht seen next))))
-
-(define (max-touch-count robots)
+(define (overlap? robots)
   (define ht
     (for/fold ([ht (make-immutable-hash)]) ([r robots])
-      (hash-update ht (first r) add1 1)))
-  (define seen (make-hash))
-  (apply max
-         (for/list ([r robots]
-                    #:do ((define key (first r))))
-           (touching-count ht seen key))))
+      (hash-update ht (first r) add1 0)))
+  (ormap (curryr > 1) (hash-values ht)))
 
-(define (part2 input w h c)
-  (match-define (list robots max-idx max)
-    (for*/fold ([accum (list input -1 0)]) ([i c])
-      (match-define (list robots max-idx max) accum)
-      (define next-robots
-        (for/list ([r robots])
-          (match-define (list p v) r)
-          (list (list (modulo (+ (first p) (first v)) w) (modulo (+ (last p) (last v)) h)) v)))
-      (define curr-max-touch-count (max-touch-count next-robots))
-      (if (> curr-max-touch-count max)
-          (list next-robots i curr-max-touch-count)
-          (list next-robots max-idx max))))
-  (add1 max-idx))
+(define (part2 input w h)
+  (for/first ([i (in-naturals)]
+              #:do ((set! input (map (curry blink w h) input)))
+              #:when (not (overlap? input)))
+    (add1 i)))
 
 (module+ test
   (require rackunit)
@@ -72,4 +44,8 @@
   (define input (fmt "static/day14input.txt"))
   (check-equal? (part1 example 11 7 100) 12)
   (check-equal? (part1 input 101 103 100) 229839456)
-  (check-equal? (part2 input 101 103 10000) 7138))
+  (check-equal? (part2 input 101 103) 7138))
+
+(module+ main
+  (define input (fmt "static/day14input.txt"))
+  (printf "day13\n\tpart1: ~a\n\tpart2: ~a\n" (part1 input 101 103 100) (part2 input 101 103)))
