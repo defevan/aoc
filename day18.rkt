b2bc04c817fe48a19531f0601d8253aecc9d026d
defevan <evanjones2024@icloud.com>
2024-12-18 00:32:07 -0800

    Don't be a dumbass.

--- day18.rkt
+++ day18.rkt
@@ -1,66 +1,68 @@
 #lang racket
 
-(require graph threading)
+(require graph
+         threading)
 
-(define (up x y) (list x (sub1 y)))
-(define (down x y) (list x (add1 y)))
-(define (right x y) (list (add1 x) y))
-(define (left x y) (list (sub1 x) y))
+(define (up x y)
+  (list x (sub1 y)))
+(define (down x y)
+  (list x (add1 y)))
+(define (right x y)
+  (list (add1 x) y))
+(define (left x y)
+  (list (sub1 x) y))
 
 (define (fmt fn)
-  (for/list ((l (file->lines fn)))
+  (for/list ([l (file->lines fn)])
     (map string->number (string-split l ","))))
 
 (define (calc input w h steps target)
   (define corrupted (take input steps))
   (define ht
-    (for*/hash ((h h) (w w)
-                      #:do ((define key (list w h)))
-                      #:when (not (member key corrupted)))
+    (for*/hash ([h h]
+                [w w]
+                #:do ((define key (list w h)))
+                #:when (not (member key corrupted)))
       (values key #t)))
-  (define edges (append*
-                 (for/list ((k (hash-keys ht)))
-                   (match-define (list x y) k)
-                   (filter (lambda (lst) (hash-has-key? ht (last lst)))
-                           (map (lambda (dir) (list (list x y) (dir x y)))
-                                (list up right down left))))))
-  (dijkstra (undirected-graph edges) (list 0 0)))
+  (define edges
+    (append* (for/list ([k (hash-keys ht)])
+               (match-define (list x y) k)
+               (filter (lambda (lst) (hash-has-key? ht (last lst)))
+                       (map (lambda (dir) (list (list x y) (dir x y))) (list up right down left))))))
+  (define-values (scores _) (dijkstra (undirected-graph edges) (list 0 0)))
+  scores)
 
 (define (part1 input w h steps target)
-  (define-values (scores paths) (calc input w h steps target))
-  (hash-ref scores target))
+  (hash-ref (calc input w h steps target) target))
 
-(define (chunk lst size)
-  (cond ((null? lst)
-         '())
-        ((< (length lst) size)
-         (list lst))
-        (else (cons (take lst size) (chunk (drop lst size) size)))))
+(define-syntax-rule (define/memo (id args ...) body ...)
+  (define id
+    ((lambda (ht)
+       (lambda (args ...)
+         (hash-ref ht
+                   (list args ...)
+                   (lambda ()
+                     (define ret
+                       (let ()
+                         body ...))
+                     (hash-set! ht (list args ...) ret)
+                     ret))))
+     (make-hash))))
 
-;; Six minute run time on input. You should reealy make this better.
-;; Optimized dijkstras + Binary search?
+(define (bin proc lst)
+  (define cursor (sub1 (floor (/ (length lst) 2))))
+  (define middle (list-ref lst cursor))
+  (define middle-value (proc middle))
+  (define next-value (proc (list-ref lst (add1 cursor))))
+  (cond
+    [(and middle-value next-value) (bin proc (drop lst cursor))]
+    [(and middle-value (not next-value)) middle]
+    [else (bin proc (take lst cursor))]))
+
 (define (part2 input w h start-steps target)
-  (define threads
-    (for/list ((c (chunk (range start-steps (sub1 (length input))) 102)))
-      (thread
-       (lambda ()
-         (define (loop step-lst)
-           (if (null? step-lst)
-               'nil
-               (let ()
-                 (define-values (scores paths) (calc input w h (car step-lst) target))
-                 (if (and (hash-has-key? scores target)
-                          (infinite? (hash-ref scores target)))
-                     (list-ref input (sub1 (car step-lst)))
-                     (loop (cdr step-lst))))))
-         (thread-send (thread-receive) (loop c))))))
-  (for/list ((t threads))
-    (thread-send t (current-thread)))
-  (~> (for/list ((t threads)) (thread-receive))
-      (filter list? _)
-      (map (lambda (v) (list v (index-of input v))) _)
-      (argmin last _)
-      (first)
+  (define/memo (proc steps) (not (infinite? (hash-ref (calc input w h steps target) target))))
+  (~> (bin proc (range start-steps (sub1 (length input))))
+      (list-ref input _)
       (map number->string _)
       (string-join ",")))
 
