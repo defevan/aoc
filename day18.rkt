df0cbee8b1fb7c675ee461667fcd9ccfda56de8d
defevan <evanjones2024@icloud.com>
2024-12-18 16:53:52 -0800

    day18: remove deps, make simpler.

--- day18.rkt
+++ day18.rkt
@@ -1,8 +1,5 @@
 #lang racket
 
-(require graph
-         threading)
-
 (define (up x y)
   (list x (sub1 y)))
 (define (down x y)
@@ -15,6 +12,32 @@
 (define (fmt fn)
   (for/list ([l (file->lines fn)])
     (map string->number (string-split l ","))))
+
+(define (directed edges start)
+  (define all (remove-duplicates (append (map first edges) (map last edges))))
+  (define scores (make-hash))
+  (for/list ([key all])
+    (define value (if (equal? key start) 0 +inf.0))
+    (hash-set! scores key value))
+  (define neighbor-ht (make-hash))
+  (for/list ([edge edges])
+    (hash-update! neighbor-ht (first edge) (lambda (lst) (cons (last edge) lst)) '()))
+  (define (loop node score)
+    (define neighbors
+      (if (hash-has-key? neighbor-ht node)
+          (hash-ref neighbor-ht node)
+          '()))
+    (for/list ([n neighbors])
+      (define neighbor-score-curr (hash-ref scores n))
+      (define neighbor-score-next (add1 score))
+      (when (< neighbor-score-next neighbor-score-curr)
+        (hash-set! scores n neighbor-score-next)
+        (loop n neighbor-score-next))))
+  (loop start 0)
+  scores)
+
+(define (undirected edges start)
+  (directed (append edges (map (lambda (edge) (list (last edge) (first edge))) edges)) start))
 
 (define (calc input w h steps target)
   (define corrupted (take input steps))
@@ -29,25 +52,10 @@
                (match-define (list x y) k)
                (filter (lambda (lst) (hash-has-key? ht (last lst)))
                        (map (lambda (dir) (list (list x y) (dir x y))) (list up right down left))))))
-  (define-values (scores _) (dijkstra (undirected-graph edges) (list 0 0)))
-  scores)
+  (undirected edges (list 0 0)))
 
 (define (part1 input w h steps target)
   (hash-ref (calc input w h steps target) target))
-
-(define-syntax-rule (define/memo (id args ...) body ...)
-  (define id
-    ((lambda (ht)
-       (lambda (args ...)
-         (hash-ref ht
-                   (list args ...)
-                   (lambda ()
-                     (define ret
-                       (let ()
-                         body ...))
-                     (hash-set! ht (list args ...) ret)
-                     ret))))
-     (make-hash))))
 
 (define (bin proc lst)
   (define cursor (sub1 (floor (/ (length lst) 2))))
@@ -60,11 +68,10 @@
     [else (bin proc (take lst cursor))]))
 
 (define (part2 input w h start-steps target)
-  (define/memo (proc steps) (not (infinite? (hash-ref (calc input w h steps target) target))))
-  (~> (bin proc (range start-steps (sub1 (length input))))
-      (list-ref input _)
-      (map number->string _)
-      (string-join ",")))
+  (define (proc steps)
+    (not (infinite? (hash-ref (calc input w h steps target) target))))
+  (define result (bin proc (range start-steps (sub1 (length input)))))
+  (string-join (map number->string (list-ref input result)) ","))
 
 (module+ test
   (require rackunit)
