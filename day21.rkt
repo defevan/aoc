e6e296ed43bdf6539cb723eb2c6c6eafb957226e
defevan <evanjones2024@icloud.com>
2024-12-21 00:58:04 -0800

    day21p2

--- day21.rkt
+++ day21.rkt
@@ -1,5 +1,19 @@
 #lang racket
 
+(define-syntax-rule (define/memo (id args ...) body ...)
+  (define id
+    ((lambda (ht)
+       (lambda (args ...)
+         (hash-ref ht
+                   (list args ...)
+                   (lambda ()
+                     (define ret
+                       (let ()
+                         body ...))
+                     (hash-set! ht (list args ...) ret)
+                     ret))))
+     (make-hash))))
+
 (define (hash-find ht find)
   (for/first ([key (hash-keys ht)]
               #:do ((define value (hash-ref ht key)))
@@ -14,90 +28,89 @@
 (define (go dir pos)
   (list ((first dir) (first pos)) ((second dir) (second pos))))
 
-(define (directed edges start)
-  (define all (remove-duplicates (append (map first edges) (map last edges))))
-  (define scores (make-hash))
-  (for/list ([key all])
-    (define value (if (equal? key start) 0 +inf.0))
-    (hash-set! scores key value))
-  (define neighbor-ht (make-hash))
-  (for/list ([edge edges])
-    (hash-update! neighbor-ht (first edge) (lambda (lst) (cons (last edge) lst)) '()))
-  (define (loop node score)
-    (define neighbors
-      (if (hash-has-key? neighbor-ht node)
-          (hash-ref neighbor-ht node)
-          '()))
-    (for/list ([n neighbors])
-      (define neighbor-score-curr (hash-ref scores n))
-      (define neighbor-score-next (add1 score))
-      (when (< neighbor-score-next neighbor-score-curr)
-        (hash-set! scores n neighbor-score-next)
-        (loop n neighbor-score-next))))
-  (loop start 0)
-  scores)
-
-;; (define (undirected edges start)
-;;  (directed (append edges (map (lambda (edge) (list (last edge) (first edge))) edges)) start))
-
 (define (fmt fn)
-  (for/list ((l (file->lines fn)))
+  (for/list ([l (file->lines fn)])
     (string->list l)))
 
-(define map1 (make-hash (list (cons '(0 0) #\7) (cons '(0 1) #\8) (cons '(0 2) #\9)
-                              (cons '(1 0) #\4) (cons '(1 1) #\5) (cons '(1 2) #\6)
-                              (cons '(2 0) #\1) (cons '(2 1) #\2) (cons '(2 2) #\3)
-                              (cons '(3 1) #\0) (cons '(3 2) #\A))))
+(define map1
+  (make-hash (list (cons '(0 0) #\7)
+                   (cons '(0 1) #\8)
+                   (cons '(0 2) #\9)
+                   (cons '(1 0) #\4)
+                   (cons '(1 1) #\5)
+                   (cons '(1 2) #\6)
+                   (cons '(2 0) #\1)
+                   (cons '(2 1) #\2)
+                   (cons '(2 2) #\3)
+                   (cons '(3 1) #\0)
+                   (cons '(3 2) #\A))))
 
-(define map2 (make-hash (list (cons '(0 1) #\^) (cons '(0 2) #\A)
-                              (cons '(1 0) #\<) (cons '(1 1) #\v) (cons '(1 2) #\>))))
-
-(define (edges ht)
-  (for*/list ([key (hash-keys ht)]
-              [dir (list up right down left)]
-              #:do ((define neighbor-pos (go dir key)))
-              #:when (hash-has-key? ht neighbor-pos))
-    (list key neighbor-pos)))
-
-(define (paths ht scores pos find)
-  (cond ((equal? pos find) (list (list pos)))
-        (else (for/list ((dir (list up right down left))
-                         #:do ((define next (go dir pos)))
-                         #:when (and (hash-has-key? scores next)
-                                     (equal? (add1 (hash-ref scores pos)) (hash-ref scores next)))
-                         (tl (paths ht scores next find)))
-                (cons pos tl)))))
+(define map2
+  (make-hash
+   (list (cons '(0 1) #\^) (cons '(0 2) #\A) (cons '(1 0) #\<) (cons '(1 1) #\v) (cons '(1 2) #\>))))
 
 (define (dirchar from to)
-  (cond ((equal? (go up from) to) #\^)
-        ((equal? (go right from) to) #\>)
-        ((equal? (go down from) to) #\v)
-        ((equal? (go left from) to) #\<)))
+  (cond
+    [(equal? (go up from) to) #\^]
+    [(equal? (go right from) to) #\>]
+    [(equal? (go down from) to) #\v]
+    [(equal? (go left from) to) #\<]))
 
-(define (combos lst)
-  (cond ((empty? lst) '(()))
-        (else (for*/list ((hd (car lst)) (tl (combos (cdr lst))))
-                (cons hd tl)))))
+(define (combos lst #:accum (accum '(())))
+  (if (empty? lst)
+      accum
+      (combos (cdr lst)
+              #:accum (append-map (lambda (sub) (map (lambda (x) (cons x sub)) (car lst))) accum))))
 
-(define (calc l m)
-  (map append*
-       (combos
-        (for/list ((s (cons #\A l)) (e l))
-          (define start (hash-find m s))
-          (define end (hash-find m e))
-          (for/list ((path (paths m (directed (edges m) start) start end)))
-            (append (for/list ((a path) (b (cdr path))) (dirchar a b)) (list #\A)))))))
+(define (bfs ht start find)
+  (define (loop queue result)
+    (cond
+      [(empty? queue) result]
+      [else
+       (define path (first queue))
+       (define curr (first path))
+       (loop (append (rest queue)
+                     (for/list ([dir (list up right down left)]
+                                #:do ((define next (go dir curr)))
+                                #:when (and (not (member next path)) (hash-has-key? ht next)))
+                       (cons next path)))
+             (if (equal? curr find)
+                 (cons (reverse path) result)
+                 result))]))
+  (loop (list (list start)) '()))
 
-(define (part1 input)
+(define (possible-paths m l)
+  (combos (reverse (for/list ([s (cons #\A l)]
+                              [e l])
+                     (define paths (bfs m (hash-find m s) (hash-find m e)))
+                     (define min-length (length (argmin length paths)))
+                     (for/list ([path paths]
+                                #:when (equal? min-length (length path)))
+                       (append (for/list ([a path]
+                                          [b (cdr path)])
+                                 (dirchar a b))
+                               (list #\A)))))))
+
+(define/memo (robots lst input)
+             (if (empty? lst)
+                 (length input)
+                 (apply min
+                        (for/list ([paths (possible-paths (car lst) input)])
+                          (for/sum ((steps paths)) (robots (cdr lst) steps))))))
+
+(define (day21 count input)
   (for/sum ((l input))
-    (define n (string->number (list->string (filter char-numeric? l))))
-    (define len (length (argmin length (for*/list ((a (calc l map1))
-                                                   (b (calc a map2))
-                                                   (c (calc b map2)))
-                                         c))))
-    (* n len)))
+           (* (string->number (list->string (filter char-numeric? l)))
+              (robots (cons map1 (make-list count map2)) l))))
 
 (module+ test
   (require rackunit)
-  (check-equal? (part1 (fmt "static/day21example.txt")) 126384)
-  (check-equal? (part1 (fmt "static/day21input.txt")) 94284))
+  (check-equal? (day21 2 (fmt "static/day21example.txt")) 126384)
+  (check-equal? (day21 2 (fmt "static/day21input.txt")) 94284)
+  (check-equal? (day21 25 (fmt "static/day21input.txt")) 116821732384052))
+
+(module+ main
+  (define input (fmt "static/day21input.txt"))
+  (printf "day21\n\tpart1: ~a\n\tpart2: ~a\n"
+          (day21 2 (fmt "static/day21input.txt"))
+          (day21 25 (fmt "static/day21input.txt"))))
