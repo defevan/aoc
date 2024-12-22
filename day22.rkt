005e4887a65c9aae64ccb736c936e4c01f4918bb
defevan <evanjones2024@icloud.com>
2024-12-22 15:43:19 -0800

    Perf improvements for day22p2.

--- day22.rkt
+++ day22.rkt
@@ -1,8 +1,14 @@
-#lang racket
+#lang racket/base
+
+(require racket/file
+         racket/list
+         racket/function
+         racket/set
+         racket/treelist
+         threading)
 
 (define (fmt fn)
-  (for/list ([l (file->lines fn)])
-    (string->number l)))
+  (map string->number (file->lines fn)))
 
 (define (step1 n)
   (modulo (bitwise-xor n (* n 64)) 16777216))
@@ -14,33 +20,28 @@
   (modulo (bitwise-xor n (* n 2048)) 16777216))
 
 (define (next n)
-  (step3 (step2 (step1 n))))
+  (~> n step1 step2 step3))
 
 (define (part1 input)
-  (for/sum ((n input))
-           (for/fold ([v n]) ([i 2000])
-             (next v))))
-
-(define (last-digit n)
-  (modulo n 10))
+  (for/sum ((n input)) (for/fold ([v n]) ([_ 2000]) (next v))))
 
 (define (part2 input)
   (define ht (make-hash)) ;; #hash((pat . count))
   (for ([n input])
-    (define prev (last-digit n))
-    (define pairlst '()) ;; '(last-digit . last-digit-diff)
-    (define seen (make-hash))
+    (define prev (modulo n 10))
+    (define pairlst (treelist)) ;; '((last-digit . last-digit-diff))
+    (define seen (mutable-set))
     (for ([i 2000])
       (set! n (next n))
-      (define curr (last-digit n))
-      (set! pairlst (append pairlst (list (cons curr (- curr prev)))))
+      (define curr (modulo n 10))
+      (set! pairlst (treelist-append pairlst (treelist (cons curr (- curr prev)))))
       (set! prev curr))
-    (for ([i (- (length pairlst) 3)])
-      (define pairs (take (drop pairlst i) 4))
-      (define pat (map cdr pairs))
-      (define count (car (last pairs)))
-      (when (not (hash-has-key? seen pat))
-        (hash-set! seen pat #t)
+    (for ([i (- (treelist-length pairlst) 3)])
+      (define pairs (treelist-take (treelist-drop pairlst i) 4))
+      (define pat (treelist-map pairs cdr))
+      (define count (car (treelist-last pairs)))
+      (unless (set-member? seen pat)
+        (set-add! seen pat)
         (hash-update! ht pat (curry + count) 0))))
   (apply max (hash-values ht)))
 
